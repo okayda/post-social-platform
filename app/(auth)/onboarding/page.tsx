f1fab5b6bc7c8e88a36a16d473a9991150d764d5
Jhon <59811681+okayda@users.noreply.github.com>
2023-08-20 02:38:28 -0700

    Add update userData type

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -9,8 +9,9 @@ async function Page() {
   const userData = {
     id: user?.id,
     objectId: userInfo?._id,
+
     username: userInfo?.username || user?.username,
-    name: userInfo?.name || user?.firstName || "",
+    name: userInfo?.name || user?.firstName,
     bio: userInfo?.bio || "",
     image: userInfo?.image || user?.imageUrl,
   };
