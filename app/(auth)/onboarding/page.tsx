2c97752d600ef5dd1f277045dfb1a7f7a75bda46
Jhon <59811681+okayda@users.noreply.github.com>
2023-08-20 04:59:06 -0700

    Add update #2 at userData

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -11,7 +11,7 @@ async function Page() {
     objectId: userInfo?._id,
 
     username: userInfo?.username || user?.username,
-    name: userInfo?.name || user?.firstName,
+    name: userInfo?.name || user?.firstName || "",
     bio: userInfo?.bio || "",
     image: userInfo?.image || user?.imageUrl,
   };
